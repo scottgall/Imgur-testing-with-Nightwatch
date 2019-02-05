f093eb327bebde5983805a54e0bc9a2039c31aa3
Scott Gall <scottarthurgall@gmail.com>
2019-02-04 21:59:09 -0600

    search and upload tests working

--- tests/search-images.spec.js
+++ tests/search-images.spec.js
@@ -1,7 +1,8 @@
 module.exports = {
   'SEARCH IMAGES TEST' : function (browser)
   {
-    const searchTerm = 'waffles' 
+    const searchTerm = 'waffles';
+    const regexSearchTerm = new RegExp(searchTerm, 'i');
     browser
       .url('http://www.imgur.com')
       .waitForElementVisible('body', 5000)
@@ -12,10 +13,13 @@ module.exports = {
         browser
           .assert.notEqual(url.value, 'https://imgur.com/')
       })
-      .assert.urlEquals(`https://imgur.com/search?q=${searchTerm}`)
+      .assert.urlEquals(`https://imgur.com/search?q=${searchTerm}`);
+    browser.expect.element('.search').value.to.be.equal(searchTerm);
     browser.expect.element('.search-sentence>span>i').text.to.not.equal('0');
     browser.expect.element('.search-term-text').text.to.equal(searchTerm);
     browser.expect.element('.cards').to.be.present;
+    browser.moveToElement('img:nth-of-type(1)', 0, 0);
+    browser.expect.element('.matched-search-term').text.to.match(regexSearchTerm);
     browser
         .pause(3000)  
         .end();
